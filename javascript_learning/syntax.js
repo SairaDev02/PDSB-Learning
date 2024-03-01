0b75843132022aab99db1d00a658aab06d1bf468
DevElias <sairadev02@gmail.com>
2024-03-01 11:26:00 +0800

    Added js files

---
+++ javascript_learning/syntax.js
@@ -0,0 +1,45 @@
+//TODO JavaScript syntax
+
+//? How to create variables
+
+var x; //global variable
+let y; //block level variable
+
+//? How to use variables
+
+x = 5;
+y = 6;
+
+let z = x + y;
+
+//? JS literals
+
+//Numbers are written with or without decimals
+10.05
+10
+
+//Strings are text written within double or single quotes
+"Jon"
+'Jon'
+
+//? JS variables
+
+var a;
+let b;
+const c = 1; //for unchanging variable values
+
+//? JS operators
+
+5 + 6 / 2 * 10 - 1
+
+let aa = 12; //assignment operator
+
+//? JS Case sensitive
+
+let varA = 1;
+let VarA = 1;
+
+//! Different variables despite same name
+
+
+
